8373e31665ed122b22f0219edff8b1134e73f409
Jarviscao <jarviscao321@gmail.com>
2023-06-14 15:08:10 +0900

    print the calculations

--- calculations.py
+++ calculations.py
@@ -1,2 +1,12 @@
 number1 = int(input("Enter number 1: "))
 number2 = int(input("Enter number 2: "))
+
+plus = number1 + number2
+minus = number1 - number2
+multiply = number1*number2
+divide = number1 / number2
+
+print(plus)
+print(minus)
+print(multiply)
+print(divide)
