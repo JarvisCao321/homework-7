d80c61efbf9e741048a74d6b1f77ada0b7e687c7
Jarviscao <jarviscao321@gmail.com>
2023-06-14 15:20:26 +0900

    add lcm function

--- calculations.py
+++ calculations.py
@@ -10,3 +10,27 @@ print(plus)
 print(minus)
 print(multiply)
 print(divide)
+
+def calc_lcm (x, y):
+
+              if x > y:
+
+                             greater = x
+
+              else:
+
+                             greater = y
+
+              while (True):
+
+                             if ((greater % x == 0) and (greater % y == 0 )):
+
+                                            lcm = greater 
+
+                                            break 
+
+                             greater += 1
+
+              return lcm 
+
+print("The LCM of the provided two numbers is", calc_lcm (number1, number2))
